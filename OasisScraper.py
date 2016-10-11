a5ec01cd9297acbe121f6c1f412cbb79256d7b35
Austin Shipley <austin.r.shipley@gmail.com>
2016-10-10 23:21:59 -0400

    added comments

--- OasisScraper.py
+++ OasisScraper.py
@@ -2,24 +2,33 @@ import urllib
 import re
 from twill.commands import *
 
+# Gather input from the user
 username = raw_input('Enter username: ')
 password = raw_input('Enter password: ')
 url = 'https://oasis-sso.publix.org/ESS/rp/0458/ESS#Taskflow.ESS:mtHome'
 
+# Go to the specified URL
 go(url)
 
+# FormValue (fv) finds the specified form, then the specified field in that form
+# and populates that field with the third argument
 fv("myForm", "username", username)
 fv("myForm", "password", password)
 
+# Submit our filled out form. Searches the form for a submit field and uses it
 submit('0')
 
+# Get the text from the page, now that we are logged in
 htmlfile = urllib.urlopen("https://oasis-sso.publix.org/ESS/rp/0458/ESS#Taskflow.ESS:mtHome")
 htmltext = htmlfile.read()
 
+# If we are logged in, this should print "Oasis", if not it will print "Login"
 regex = '<title>(.*)</title>'
 pattern = re.compile(regex)
 print re.findall(pattern,htmltext)
 
+# This will retrieve the string for when our next shift is
+# of the format Thursday 11:30am - 8:30pm
 regex = '<div class="x-grid3-cell-inner x-grid3-col-Shift" unselectable="on">(.+?)</div>'
 
 pattern = re.compile(regex)
--- OasisWebScraper.py
+++ OasisWebScraper.py
@@ -3,6 +3,7 @@ import urllib2
 import cookielib
 import re
 
+# Login function, not exactly sure how this works yet
 def login(url, data):
     response = opener.open(url, data)
     the_page = response.read()
@@ -15,23 +16,30 @@ url = 'https://oasis-sso.publix.org/ESS/rp/0458/ESS#Taskflow.ESS:mtHome'
 values = {'username' : username,
           'password' : password}
 
+# Encode our data and gather our cookies
 data = urllib.urlencode(values)
 cookies = cookielib.CookieJar()
 
+# Open a session, unsure what exactly this does
 opener = urllib2.build_opener(
     urllib2.HTTPRedirectHandler(),
     urllib2.HTTPHandler(debuglevel=0),
     urllib2.HTTPSHandler(debuglevel=0),
     urllib2.HTTPCookieProcessor(cookies))
 
+# Call login twice, once to establish cookies, once to login
 login(url, data)
 login(url, data)
 
+# Gather html data
 htmlfile = urllib.urlopen("https://oasis-sso.publix.org/ESS/rp/0458/ESS#Taskflow.ESS:mtHome")
 htmltext = htmlfile.read()
 
+# We are interested in the date and time string that the regex will pull
+# it will be in the format Thursday 11:30am - 8:00pm
 regex = '<div class="x-grid3-cell-inner x-grid3-col-Shift" unselectable="on">(.+?)</div>'
 pattern = re.compile(regex)
 
+# Find and store the time when our shift starts and ends, and print it
 shift = re.findall(pattern,htmltext)
 print shift
